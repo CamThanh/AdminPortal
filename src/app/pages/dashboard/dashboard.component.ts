a7920d5fe3e0c9c7deb004894e424db8d0c6c5b8
HiepHoangNguyen <hiephoang.nguyen.hcm@gmail.com>
2018-10-18 16:03:03 +0700

    Init admin dashboard

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+interface CardSettings {
+  title: string;
+  iconClass: string;
+  type: string;
+}
+
+@Component({
+  selector: 'ngx-dashboard',
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent {
+
+}
